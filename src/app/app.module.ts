0d9d6f9b41901bbaddf23b2b0b05142e9825fe02
Mozartted <mozart.osita@gmail.com>
2017-03-25 18:32:26 +0100

    base Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { MapComponent } from './map/map.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    MapComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

b87bdb4ddbba3acb9eb8244cc74328c008aec678
Mozartted <mozart.osita@gmail.com>
2017-03-26 07:28:23 +0100

    Map directives to integrate

---
+++ src/app/google-maps.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { GoogleMapsDirective } from './google-maps.directive';
+
+describe('GoogleMapsDirective', () => {
+  it('should create an instance', () => {
+    let directive = new GoogleMapsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
